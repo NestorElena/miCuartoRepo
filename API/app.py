075a056109e2eb3af406425e3ff06702c31bd11f
artfware <2123200429@soy.utj.edu.mx>
2025-05-01 02:07:02 -0600

    método de platzi agregado

--- API/app.py
+++ API/app.py
@@ -10,7 +10,14 @@ def get_familia():
     rows = ["Amin", "Marce", "Miranda"]
     return rows
 
+
 @app.get("/superheroesDC")
 def get_superheroes():
     rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    return rows
+
+
+@app.get("/Programas")
+def get_promagas():
+    rows = ["Docker", "Bash", "Linux", "Python", "Java", "Javascript"]
     return rows
